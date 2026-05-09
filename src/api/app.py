e845c77a5e413bb80db08b50185d65e1f9fc2d98
ShanmathySanjay <shanmathy2016@gmail.com>
2026-05-09 13:31:25 +0530

    yaml files

--- src/api/app.py
+++ src/api/app.py
@@ -1,10 +1,18 @@
+import logging
+import pandas as pd
 from fastapi import FastAPI
 import joblib
 import numpy as np
+from prometheus_client import Counter, generate_latest
+from fastapi.responses import Response
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+REQUEST_COUNT = Counter("request_count", "Total prediction requests")
 
 app = FastAPI()
 
-# Load pipeline (includes preprocessing + model)
 model = joblib.load("rf_model.pkl")
 
 
@@ -15,15 +23,34 @@ def home():
 
 @app.post("/predict")
 def predict(data: dict):
-    """
-    Input:
-    {
-        "features": [63, 1, 1, 145, 233, 1, 2, 150, 0, 2.3, 3, 0, 6]
-    }
-    """
+    try:
+        features = data["features"]
 
-    features = np.array(data["features"]).reshape(1, -1)
+        columns = [
+            "age", "sex", "cp", "trestbps", "chol", "fbs",
+            "restecg", "thalach", "exang", "oldpeak",
+            "slope", "ca", "thal"
+        ]
 
-    prediction = model.predict(features)
+        df = pd.DataFrame([features], columns=columns)
 
-    return {"prediction": int(prediction[0])}
+        prediction = model.predict(df)[0]
+        probability = model.predict_proba(df)[0][1]
+
+        logger.info(f"Input: {features}")
+        logger.info(f"Prediction: {prediction}, Confidence: {probability}")
+
+        REQUEST_COUNT.inc()
+
+        return {
+            "prediction": int(prediction),
+            "confidence": float(probability)
+        }
+
+    except Exception as e:
+        logger.error(f"Error: {str(e)}")
+        return {"error": str(e)}
+
+@app.get("/metrics")
+def metrics():
+    return Response(generate_latest(), media_type="text/plain")
